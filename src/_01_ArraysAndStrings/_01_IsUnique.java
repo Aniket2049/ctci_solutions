f12a2e243749d6861700234f46f9eb66972f7860
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-05-08 00:14:33 +0530

    add IsUnique

---
+++ src/_01_ArraysAndStrings/_01_IsUnique.java
@@ -0,0 +1,16 @@
+package _01_ArraysAndStrings;
+
+// Implement an algorithm to determine if a string has all unique characters. 
+// What if you cannot use additional data structures?
+public class _01_IsUnique {
+	private boolean isUnique(String str) {
+		boolean[] charSet = new boolean[128];
+		for (int i = 0; i < str.length(); i++) {
+			int val = str.charAt(i);
+			if (charSet[val])
+				return false;
+			charSet[val] = true;
+		}
+		return true;
+	}
+}
